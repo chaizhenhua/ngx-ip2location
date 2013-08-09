0ce705de3e0625166858fa7f6ed64652f2f4dd2a
chaizhenhua <chaizhenhua@gmail.com>
2013-08-09 17:27:15 +0800

    Initial commit

---
+++ test.php
@@ -0,0 +1,24 @@
+<?php
+
+echo "COUNTRY_SHORT => \"". $_SERVER{'COUNTRY_SHORT'} . "\"<br/>";
+echo "COUNTRY_LONG => \"". $_SERVER{'COUNTRY_LONG'} ."\"<br/>";
+echo "REGION => \"". $_SERVER{'REGION'} ."\"<br/>";
+echo "CITY => \"". $_SERVER{'CITY'} ."\"<br/>";
+echo "ISP => \"". $_SERVER{'ISP'} ."\"<br/>";
+echo "LATITUDE => \"". $_SERVER{'LATITUDE'} ."\"<br/>";
+echo "LONGITUDE => \"". $_SERVER{'LONGITUDE'} ."\"<br/>";
+echo "DOMAIN => \"". $_SERVER{'DOMAIN'} ."\"<br/>";
+echo "ZIPCODE => \"". $_SERVER{'ZIPCODE'} ."\"<br/>";
+echo "TIMEZONE => \"". $_SERVER{'TIMEZONE'} ."\"<br/>";
+echo "NETSPEED => \"". $_SERVER{'NETSPEED'} ."\"<br/>";
+echo "IDDCODE => \"". $_SERVER{'IDDCODE'} ."\"<br/>";
+echo "AREACODE => \"". $_SERVER{'AREACODE'} ."\"<br/>";
+echo "WEATHERSTATIONCODE => \"". $_SERVER{'WEATHERSTATIONCODE'} ."\"<br/>";
+echo "WEATHERSTATIONNAME => \"". $_SERVER{'WEATHERSTATIONNAME'} ."\"<br/>";
+echo "MCC => \"". $_SERVER{'MCC'} ."\"<br/>";
+echo "MNC => \"". $_SERVER{'MNC'} ."\"<br/>";
+echo "MOBILEBRAND => \"". $_SERVER{'MOBILEBRAND'} ."\"<br/>";
+echo "ELEVATION => \"". $_SERVER{'ELEVATION'} ."\"<br/>";
+echo "USAGETYPE => \"". $_SERVER{'USAGETYPE'} ."\"<br/>";
+
+?>
